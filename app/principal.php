b93c7a46acd5f58fb04ac37a5f637deb0ced2200
llopezm <llopez@gconfisur.com>
2022-03-04 10:32:14 -0400

    actulizacion-PostFinal02 appNueva 04/03/22

--- app/principal.php
+++ app/principal.php
@@ -17,7 +17,7 @@ require_once("../config/conexion.php");
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0 text-dark">Dashboard</h1>
+                        <h1 class="m-0 text-dark">DISTRIBUCIONES AJ, C.A</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
